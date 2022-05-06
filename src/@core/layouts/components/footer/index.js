eca228ae681ce58b3fcf386bc99782bf7738a24d
JahanviJnext <jahanvi.jnext@gmail.com>
2022-05-06 18:31:35 +0530

    Top bar and Footer Completed

--- src/@core/layouts/components/footer/index.js
+++ src/@core/layouts/components/footer/index.js
@@ -1,21 +1,26 @@
 // ** Icons Import
 import { Heart } from 'react-feather'
+import FooterLogo from '@src/assets/images/logo/gimalogo.png'
 
 const Footer = () => {
+  const date = new Date().getFullYear()
   return (
     <p className='clearfix mb-0'>
       <span className='float-md-left d-block d-md-inline-block mt-25'>
-        COPYRIGHT © {new Date().getFullYear()}{' '}
-        <a href='https://1.envato.market/pixinvent_portfolio' target='_blank' rel='noopener noreferrer'>
-          Pixinvent
+       <img src={FooterLogo} width="20px"/>  © {date}{" "} 
+        <a href='https://1.envato.market/pixinvent_portfolio' target='_blank' rel='noopener noreferrer' style={{color:"#8DC454"}}>
+          DDEP
         </a>
-        <span className='d-none d-sm-inline-block'>, All rights Reserved</span>
+        <span className='d-none d-sm-inline-block'> - powered by a4appz Limited. Member of SiiA Group, All rights Reserved</span>
       </span>
-      <span className='float-md-right d-none d-md-block'>
+      {/* <span className='float-md-right d-none d-md-block'>
         Hand-crafted & Made with
         <Heart size={14} />
+      </span> */}
+      <span className='float-md-right d-none d-md-block'>
+        Production Site -Version #V3(V10)
       </span>
-    </p>
+    </p> 
   )
 }
 
