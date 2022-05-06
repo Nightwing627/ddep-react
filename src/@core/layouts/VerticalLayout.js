1ac2d779b062c5f9ca79df61409ee3c3ab128ba5
JahanviJnext <jahanvi.jnext@gmail.com>
2022-05-06 18:32:07 +0530

    Top bar and Footer Complete

--- src/@core/layouts/VerticalLayout.js
+++ src/@core/layouts/VerticalLayout.js
@@ -160,7 +160,7 @@ const VerticalLayout = props => {
           `header-navbar navbar align-items-center ${navbarClasses[navbarType] || 'floating-nav'} navbar-shadow`
         )}
       >
-        <div className='navbar-container d-flex content'>
+        <div className='navbar-container d-flex justify-content-end content'>
           {navbar ? navbar : <NavbarComponent setMenuVisibility={setMenuVisibility} skin={skin} setSkin={setSkin} />}
         </div>
       </Navbar>
--- src/@core/layouts/components/navbar/NavbarUser.js
+++ src/@core/layouts/components/navbar/NavbarUser.js
@@ -1,18 +1,20 @@
 // ** Dropdowns Imports
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 
 import UserDropdown from './UserDropdown'
+import "../navbar/navbar.scss"
 
 // ** Third Party Components
 import { Sun, Moon, Menu } from 'react-feather'
-import { NavItem, NavLink } from 'reactstrap'
-
+import { NavItem, NavLink, Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap' 
+import ReactCountryFlag from "react-country-flag"
 const NavbarUser = props => {
   // ** Props
   const { skin, setSkin, setMenuVisibility } = props
-
+  const [toggler, setToggler] = useState(false)
   // ** Function to toggle Theme (Light/Dark)
   const ThemeToggler = () => {
+   
     if (skin === 'dark') {
       return <Sun className='ficon' onClick={() => setSkin('light')} />
     } else {
@@ -20,6 +22,9 @@ const NavbarUser = props => {
     }
   }
 
+  const handleToggle = () => {
+    setToggler(!toggler)
+  }
   return (
     <Fragment>
       <ul className='navbar-nav d-xl-none d-flex align-items-center'>
@@ -29,14 +34,55 @@ const NavbarUser = props => {
           </NavLink>
         </NavItem>
       </ul>
-      <div className='bookmark-wrapper d-flex align-items-center'>
-        <NavItem className='d-none d-lg-block'>
-          <NavLink className='nav-link-style'>
+      {/* <div className='bookmark-wrapper align-items-center'>
+        <NavItem className='d-none d-lg-block float-right'>
+          <NavLink className='nav-link-style ' style={{color:"#fff"}}>
             <ThemeToggler />
           </NavLink>
         </NavItem>
-      </div>
-      <ul className='nav navbar-nav align-items-center ml-auto'>
+      </div> */}
+      <div>
+  
+  <Dropdown
+    isOpen={toggler}
+     toggle={() => handleToggle()} 
+     className="dropdwon-menu"
+  >
+    <DropdownToggle caret>
+    
+
+    <ReactCountryFlag className="country-flag" countryCode="us"  style={{
+                    width: '1.5em',
+                    height: '2em'
+                }} svg />
+                    <span className="ml-1 country-name">English</span>
+    </DropdownToggle>
+    <DropdownMenu >
+      <DropdownItem >
+      <ReactCountryFlag className="country-flag" countryCode="fr" style={{
+                    width: '1.5em',
+                    height: '2em'
+                }} svg />
+                    <span className="ml-1 country-name">French</span>
+      </DropdownItem>
+      <DropdownItem >
+      <ReactCountryFlag className="country-flag" countryCode="de" style={{
+                    width: '1.5em',
+                    height: '2em'
+                }} svg />
+                    <span className="ml-1 country-name">German</span>
+      </DropdownItem>
+      <DropdownItem >
+      <ReactCountryFlag className="country-flag" countryCode="pt" style={{
+                    width: '1.5em',
+                    height: '2em'
+                }} svg />
+                    <span className="ml-1 country-name">Portuguese</span>
+      </DropdownItem>
+    </DropdownMenu>
+  </Dropdown>
+</div>
+      <ul className='nav navbar-nav align-items-center float-right'>
         <UserDropdown />
       </ul>
     </Fragment>
